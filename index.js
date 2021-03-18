8fcf7f32e3af61c95bcf2a16f51367f7f6cc1b60
MKIsHereOficial <mkishere.oficial@gmail.com>
2021-03-18 17:14:10 +0000

    a

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Discord = require('discord.js'), client = new Discord.Client();
 const imports = {
   luxon: require('luxon'),
@@ -5,37 +7,58 @@ const imports = {
 }
 const luxon = imports.luxon;
 const db = imports.db;
+const rl = require('readline-sync');
 
 client.imports = imports;
 
+const LUXON_ZONE = new luxon.Zone();
+LUXON_ZONE.universal = true;
+
 //////////////////////////////////////////////////////
 
-let BOT_STARTED_AT = new Date();
+function logErr (message) {
+    if (!message) return null;
+    return message;
+}
+async function sendMessageDM(user) {
+    if (!user) return;
+    let USER_MESSAGE_INDEX = 0;
+    //console.log(`[${USER_MESSAGE_INDEX}] Apagando histórico de mensagem para ${user.tag} [...]`);
+    //await user.deleteDM();
+    console.log(`[${USER_MESSAGE_INDEX}] Aguardando mensagem para ${user.tag} [...]`)
+    const message = rl.question(`Mensagem para ${user.tag}: `);
+    console.log(`[${USER_MESSAGE_INDEX}] Enviando mensagem para ${user.tag} [...]`)
+    await user.send(message).catch(logErr).then(async msg => {
+        console.log(`[${USER_MESSAGE_INDEX}] Mensagem enviada para ${user.tag}`)
+    });
+}
+
+let BOT_STARTED_AT = luxon.DateTime.fromJSDate(new Date());
 client.on('ready', () => {
-  BOT_STARTED_AT = new Date();
-  let t = BOT_STARTED_AT;
-  t = {
-    original: t,
-    hour: (async => {
-      time = t.getHours();
-      time = (time < 10) ? `0${time}` : `${time}`;
-      return time;
-    })(),
-    minute: (async => {
-      time = t.getMinutes();
-      time = (time < 10) ? `0${time}` : `${time}`;
-      return time;
-    })(),
-    second: (async => {
-      time = t.getSeconds();
-      time = (time < 10) ? `0${time}` : `${time}`;
-      return time;
-    })()
-  }
-  t.fullTime = () => {return `${t.hour}:${t.minute}:${t.second}`;}
-  let a = new luxon.DateTime();
-  console.log(`Loguei-me como ${client.user.tag} às ${a.toFormat("hh:mm:ss")}`);
+  BOT_STARTED_AT = luxon.DateTime.fromJSDate(new Date());
+  let a = BOT_STARTED_AT;
+  a = a.setZone("America/Sao_Paulo");
+  console.log(`Loguei-me como ${client.user.tag} às ${a.toFormat("HH:mm:ss")}`);
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+  
+
+    client.users.cache.map(async user => {
+        if (user.id === "771506901850652693") {
+            await sendMessageDM(user);
+        }
+    })
+  ////////////////////////////////////////////////////////////////////////////////
 });
+
+client.on('message', async message => {
+    if (message.author.bot) return console.log(`[${message.author.tag}]:`, `${message.cleanContent}`);
+    if (!message.guild && !message.author.bot) {
+        console.log(`[${message.author.tag}]:`, `${message.cleanContent}`);
+        return await sendMessageDM(message.author);
+    }
+})
 
 //////////////////////////////////////////////////////
 
